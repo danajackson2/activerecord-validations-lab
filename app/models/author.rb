6c319bce9f73e467372815efcb744959cda477eb
danajackson2 <danajackson2@gmail.com>
2020-12-17 16:53:58 -0800

    validation baby

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ActiveRecord::Base
+    validates_presence_of :name
+    validates_uniqueness_of :name
+    validates_length_of :phone_number, is:10
+    
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+    validates_presence_of :title
+    validates :content, length: {minimum:250}
+    validates :summary, length: {maximum:250}
+    validates :category, inclusion: {in:["Fiction", "Non-Fiction"]}
+    validate :clickbait
+
+    def clickbait
+        if self.title == nil || self.title[/Won't Believe|Secret|Top|Guess/] == nil
+            errors.add(:clickbait, "Not clickbaity enough")
+        end
+    end
+
 end
